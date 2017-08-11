3da4769549380d9e6778a8540c8e57be8d233d8a
Alexis CS <12alexiscs+dev@gmail.com>
2017-08-11 15:06:08 -0500

    Escena inicio

---
+++ Sandwichmania/Assets/Scripts/Escena_Inicio/Admin_level0.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Admin_level0 : MonoBehaviour {
+	public InputField id;
+	public InputField password;
+	public Text message;
+
+	public void Setget (){
+		if (!(id.text.ToString().Equals ("alexis")) || !(password.text.ToString ().Equals ("contraseña"))) {
+			message.text = "Usuario o contraseña incorrecta";
+		} else {
+			SceneManager.LoadScene (1);
+		}
+	}
+	 
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
