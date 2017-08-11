809c85b7471f9387d3a3bfeaeba8d6940fc8056c
Alexis CS <12alexiscs+dev@gmail.com>
2017-08-11 13:14:27 -0500

    Inicio
    El juego como lo dejaron

---
+++ Sandwichmania/Assets/Scripts/Menu_Ini_Btons.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Menu_Ini_Btons : MonoBehaviour {
+
+	public GameObject[] music;
+	public static int mano;
+	private AudioSource manejadorSonido;
+
+	void Awake() {
+		DontDestroyOnLoad(music[0]);
+	}
+
+	// Use this for initialization
+	void Start () {
+		NotificationCenter.DefaultCenter().AddObserver(this, "altoMusicaInicio");
+		manejadorSonido = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void cargaEscena(int valor)
+	{
+		Application.LoadLevel (1);
+		mano = valor;
+	}
+
+	public void altoMusicaInicio(){
+		manejadorSonido.Stop ();
+	}
+}
---
+++ Sandwichmania/Assets/Scripts/Muestra_Img.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Muestra_Img : MonoBehaviour {
+
+	public GameObject[] obj;
+	public float tiempoMin = 10.0f;
+	public float tiempoMax = 12.0f;
+	private int teclaPresiona = 10;
+	// Use this for initialization
+	void Start () {
+		Muestra(0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.UpArrow) && (teclaPresiona==0) ) {
+			Muestra (1);
+			GetComponent<AudioSource>().Play();
+		}
+		if (Input.GetKey (KeyCode.UpArrow) && (teclaPresiona==1)) {
+			Muestra (2);
+			GetComponent<AudioSource>().Play();
+		}
+		if (Input.GetKey (KeyCode.DownArrow) && (teclaPresiona==2)) {
+			Muestra (3);
+			GetComponent<AudioSource>().Play();
+		}
+		if (Input.GetKey (KeyCode.LeftArrow) && (teclaPresiona==3)) {
+			Muestra (4);
+			GetComponent<AudioSource>().Play();
+		}
+		if (Input.GetKey (KeyCode.RightArrow) && (teclaPresiona == 4)) {
+			Muestra (1);
+			GetComponent<AudioSource> ().Play ();
+		} 
+	}
+
+	void Muestra(int dedo){
+		GameObject clone = (GameObject) Instantiate (obj [dedo], transform.position, Quaternion.identity);
+		Destroy (clone, 20.0f);
+		teclaPresiona = dedo;
+		Camera.main.transform.position = new Vector3 (Camera.main.transform.position.x, Camera.main.transform.position.y, 
+		                                              Camera.main.transform.position.z - 0.0001f);
+	}
+}
