7956ac95bc94c232724ad4a4e33e6e450b1ebc4e
Alexis CS <12alexiscs+dev@gmail.com>
2017-08-16 23:38:02 -0500

    Programacion Seleccionar nivel

---
+++ Sandwichmania/Assets/Scripts/Inicio/Admin_level0.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using NUnit.Framework.Internal.Filters;
+
+public class Admin_level0 : MonoBehaviour {
+	public InputField id;
+	public InputField password;
+	public Text message;
+
+	public void Setget (){
+		if (!(id.text.ToString().Equals ("alexis")) || !(password.text.ToString ().Equals ("contraseña"))) {
+			message.text = "Usuario o contraseña incorrecta";
+		} else {
+			SceneManager.LoadScene (1);
+		}
+	} 
+		
+	//Salir y 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Sandwichmania/Assets/Scripts/Selección_Nivel/AdminPanel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AdminPanel : MonoBehaviour {
+	public GameObject[] panels;
+	// Use this for initialization
+	public void Nivel (){
+		DestroyObject (panels[0]);
+		panels [1].SetActive (true);
+	}
+		
+	public void Return (){
+		
+	
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
