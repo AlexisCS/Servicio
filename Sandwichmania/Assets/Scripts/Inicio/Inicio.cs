869de9d9b4fb7b1e637940972226df67b0c8932d
Alexis CS <12alexiscs+dev@gmail.com>
2018-01-16 17:30:28 -0600

    Inicio acabado

---
+++ Sandwichmania/Assets/Scripts/Inicio/Inicio.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Inicio : MonoBehaviour {
+	public GameObject creditosPanel;
+
+
+	public void MostrarCreditos (){
+		creditosPanel.gameObject.SetActive (true);
+	}
+
+	public void SalirCreditos(){
+		creditosPanel.gameObject.SetActive (false);
+	}
+
+	public void Jugar() {
+		//SceneManager.LoadScene ("Login");
+	}
+
+	void Awake(){
+		creditosPanel.gameObject.SetActive (false);
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
